003999da8af08e61b9327c798e567c2b0a548699
pilarfernandezz <38540218+pilarfernandezz@users.noreply.github.com>
2020-04-16 21:50:08 -0300

    Add files via upload

--- Trab2/MySignature.java
+++ Trab2/MySignature.java
@@ -1,47 +1,132 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-import MySignatureStatus;
+import java.security.*;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class MySignature {
-    private MySignatureStatus status;
-    private String value;
+    static String pattern;
+    static MessageDigest md;
+    static Key privateKey;
+    static Key publicKey;
+    static Key ey;
+    static Cipher cipher;
+    static byte[] signature;
 
-    public MySignatureStatus getStatus() {
-        return this.status;
+    public static String getPattern() {
+        return MySignature.pattern;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public static void setPattern(String pattern) {
+        MySignature.pattern = pattern;
     }
 
-    public MySignatureStatus getValue() {
-        return this.value;
+    public static MessageDigest getMD() {
+        return MySignature.md;
     }
 
-    public void setValue(String value) {
-        this.value = value;
+    public static void setMD(MessageDigest md) {
+        MySignature.md = md;
     }
 
-    public MySignature getInstance() {
+    public static Key getPublicKey() {
+        return MySignature.publicKey;
+    }
+
+    public static void setPublicKey(Key publicKey) {
+        MySignature.publicKey = publicKey;
+    }
+
+    public static Key getPrivateKey() {
+        return MySignature.privateKey;
+    }
+
+    public static void setPrivateKey(Key privateKey) {
+        MySignature.privateKey = privateKey;
+    }
+
+    public static Cipher getCipher() {
+        return MySignature.cipher;
+    }
+
+    public static void setCipher(Cipher cipher) {
+        MySignature.cipher = cipher;
+    }
+
+    public static byte[] getSignature() {
+        return MySignature.signature;
+    }
+
+    public static void setSignature(byte[] signature) {
+        MySignature.signature = signature;
+    }
+
+    public static MySignature getInstance(String pattern) {
+        MySignature.setPattern(pattern);
         return new MySignature();
     }
 
-    public void initSign() {
-        throw NotImplementedException();
-    }git
+    public static void initSign(String msg) throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
 
-    public void update() {
-        throw NotImplementedException();
+        MySignature.setPublicKey(kp.getPublic());
+        MySignature.setPrivateKey(kp.getPrivate());
+
+        System.out.println("Gerando chave pública: " + MySignature.getPublicKey());
+        System.out.println("Gerando chave privada: " + MySignature.getPrivateKey() + '\n');
+
+        MySignature.setMD(MessageDigest.getInstance(MySignature.pattern));
     }
 
-    public void initVerify() {
-        throw NotImplementedException();
+    public static void update(String msg) {
+        System.out.println("Gerando o digest da mensagem... ");
+
+        MySignature.getMD().update(msg.getBytes());
+        byte[] digest = MySignature.getMD().digest();
+        System.out.println(digest);
+
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0; i < digest.length; i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+
+        System.out.println("Digest gerado: " + hexString.toString() + '\n');
+
     }
 
-    public void verify() {
-        throw NotImplementedException();
+    public static void initVerify() throws InvalidKeyException {
+        System.out.println("Iniciando a verificação da assinatura...");
+        MySignature.getCipher().init(Cipher.DECRYPT_MODE, MySignature.publicKey);
     }
 
-    public void sign() {
-        throw NotImplementedException();
+    public static void verify() throws IllegalBlockSizeException, BadPaddingException {
+        byte[] digestAfterSignature = MySignature.getCipher().doFinal(signature);
+
+        for (int i = 0; i < MySignature.getMD().digest().length; i++) {
+            if (digestAfterSignature[i] != MySignature.getMD().digest()[i]) {
+                System.out.println("Assinatura invalida" + '\n');
+                return;
+            }
+        }
+        System.out.println("Assinatura válida" + '\n');
+    }
+
+    public static void sign() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+        System.out.println("Assinando digitalmente a mensagem...");
+
+        StringBuffer hexString = new StringBuffer();
+
+        MySignature.setCipher(Cipher.getInstance("RSA"));
+        MySignature.getCipher().init(Cipher.ENCRYPT_MODE, MySignature.privateKey);
+        MySignature.setSignature(MySignature.getCipher().doFinal(MySignature.getMD().digest()));
+        for (int i = 0; i < MySignature.getSignature().length; i++) {
+            hexString.append(Integer.toHexString(0xFF & MySignature.getSignature()[i]));
+        }
+        System.out.println("Assinatura finalizada: " + hexString.toString() + '\n');
     }
 }
--- Trab2/trab2.java
+++ Trab2/trab2.java
@@ -11,74 +11,32 @@ public class trab2 {
         String message = sc.nextLine();
         System.out.println(
                 "Digite o padrão de assinatura:\n 1 - MD5withRSA\n 2 - SHA1withRSA\n 3 - SHA256withRSA\n 4 - SHA512withRSA");
-        int signatureTyped = sc.nextInt();
-        String signatureType = null;
+        int typed = sc.nextInt();
+        String pattern = null;
 
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-
-        kpg.initialize(2048);
-        KeyPair kp = kpg.generateKeyPair();
-
-        Key pub = kp.getPublic();
-        Key pvt = kp.getPrivate();
-
-        System.out.println("CHAVE PUBLICA \n" + pub);
-        System.out.println("CHAVE PRIVADA \n" + pvt);
-
-        switch (signatureTyped) {
+        switch (typed) {
             case 1:
-                signatureType = "MD5";
+                pattern = "MD5";
                 break;
             case 2:
-                signatureType = "SHA-1";
+                pattern = "SHA-1";
                 break;
             case 3:
-                signatureType = "SHA-256";
+                pattern = "SHA-256";
                 break;
             case 4:
-                signatureType = "SHA-512";
+                pattern = "SHA-512";
                 break;
             default:
                 System.out.println("Padrão de assinatura inválido.");
                 System.exit(1);
         }
 
-        // Creating the MessageDigest object
-        MessageDigest md = MessageDigest.getInstance(signatureType);
-
-        // Passing data to the created MessageDigest Object
-        md.update(message.getBytes());
-
-        // Compute the message digest
-        byte[] digest = md.digest();
-        System.out.println(digest);
-
-        // Converting the byte array in to HexString format
-        StringBuffer hexString = new StringBuffer();
-
-        for (int i = 0; i < digest.length; i++) {
-            hexString.append(Integer.toHexString(0xFF & digest[i]));
-        }
-        System.out.println("Hex format : " + hexString.toString());
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, pvt);
-        byte[] signature = cipher.doFinal(digest);
-        for (int i = 0; i < signature.length; i++) {
-            hexString.append(Integer.toHexString(0xFF & signature[i]));
-        }
-        System.out.println("hexString - asssinatura finalizada " + hexString.toString());
-
-        // VALIDAÇÃO DA SIGNATURE
-        cipher.init(Cipher.DECRYPT_MODE, pub);
-
-        byte[] digest2 = cipher.doFinal(signature);
-
-        for (int i = 0; i < digest.length; i++) {
-            if (digest[i] != digest2[i]) {
-                System.out.println("to triste no to feliz");
-                break;
-            }
-        }
-        System.out.println("YAAAAAAAAAY porraaaa");
+        MySignature.getInstance(pattern);
+        MySignature.initSign(message);
+        MySignature.update(message);
+        MySignature.sign();
+        MySignature.initVerify();
+        MySignature.verify();
     }
 }
