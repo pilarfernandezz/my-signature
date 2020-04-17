73eb23b84c585541c48b8c663dba6ec3854c3128
pilarfernandezz <38540218+pilarfernandezz@users.noreply.github.com>
2020-04-16 21:53:45 -0300

    Add files via upload

--- Trab2/MySignature.java
+++ Trab2/MySignature.java
@@ -1,5 +1,4 @@
 import java.security.*;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -14,6 +13,74 @@ public class MySignature {
     static Cipher cipher;
     static byte[] signature;
 
+    public static MySignature getInstance(String pattern) {
+        MySignature.setPattern(pattern);
+        return new MySignature();
+    }
+
+    public static void initSign(String msg) throws NoSuchAlgorithmException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair kp = kpg.generateKeyPair();
+
+        MySignature.setPublicKey(kp.getPublic());
+        MySignature.setPrivateKey(kp.getPrivate());
+
+        System.out.println("Gerando chave pública: " + MySignature.getPublicKey());
+        System.out.println("Gerando chave privada: " + MySignature.getPrivateKey() + '\n');
+
+        MySignature.setMD(MessageDigest.getInstance(MySignature.pattern));
+    }
+
+    public static void update(String msg) {
+        System.out.println("Gerando o digest da mensagem... ");
+
+        MySignature.getMD().update(msg.getBytes());
+        byte[] digest = MySignature.getMD().digest();
+        System.out.println(digest);
+
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0; i < digest.length; i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+
+        System.out.println("Digest gerado: " + hexString.toString() + '\n');
+    }
+
+    public static void initVerify() throws InvalidKeyException {
+        System.out.println("Iniciando a verificação da assinatura...");
+        MySignature.getCipher().init(Cipher.DECRYPT_MODE, MySignature.publicKey);
+    }
+
+    public static void verify() throws IllegalBlockSizeException, BadPaddingException {
+        byte[] digestAfterSignature = MySignature.getCipher().doFinal(signature);
+
+        for (int i = 0; i < MySignature.getMD().digest().length; i++) {
+            if (digestAfterSignature[i] != MySignature.getMD().digest()[i]) {
+                System.out.println("Assinatura invalida" + '\n');
+                return;
+            }
+        }
+        System.out.println("Assinatura válida" + '\n');
+    }
+
+    public static void sign() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+        System.out.println("Assinando digitalmente a mensagem...");
+
+        StringBuffer hexString = new StringBuffer();
+
+        MySignature.setCipher(Cipher.getInstance("RSA"));
+        MySignature.getCipher().init(Cipher.ENCRYPT_MODE, MySignature.privateKey);
+        MySignature.setSignature(MySignature.getCipher().doFinal(MySignature.getMD().digest()));
+        for (int i = 0; i < MySignature.getSignature().length; i++) {
+            hexString.append(Integer.toHexString(0xFF & MySignature.getSignature()[i]));
+        }
+        System.out.println("Assinatura finalizada: " + hexString.toString() + '\n');
+    }
+
+    // ***************** GETTERS AND SETTERS *****************//
     public static String getPattern() {
         return MySignature.pattern;
     }
@@ -61,72 +128,5 @@ public class MySignature {
     public static void setSignature(byte[] signature) {
         MySignature.signature = signature;
     }
-
-    public static MySignature getInstance(String pattern) {
-        MySignature.setPattern(pattern);
-        return new MySignature();
-    }
-
-    public static void initSign(String msg) throws NoSuchAlgorithmException {
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(2048);
-        KeyPair kp = kpg.generateKeyPair();
-
-        MySignature.setPublicKey(kp.getPublic());
-        MySignature.setPrivateKey(kp.getPrivate());
-
-        System.out.println("Gerando chave pública: " + MySignature.getPublicKey());
-        System.out.println("Gerando chave privada: " + MySignature.getPrivateKey() + '\n');
-
-        MySignature.setMD(MessageDigest.getInstance(MySignature.pattern));
-    }
-
-    public static void update(String msg) {
-        System.out.println("Gerando o digest da mensagem... ");
-
-        MySignature.getMD().update(msg.getBytes());
-        byte[] digest = MySignature.getMD().digest();
-        System.out.println(digest);
-
-        StringBuffer hexString = new StringBuffer();
-
-        for (int i = 0; i < digest.length; i++) {
-            hexString.append(Integer.toHexString(0xFF & digest[i]));
-        }
-
-        System.out.println("Digest gerado: " + hexString.toString() + '\n');
-
-    }
-
-    public static void initVerify() throws InvalidKeyException {
-        System.out.println("Iniciando a verificação da assinatura...");
-        MySignature.getCipher().init(Cipher.DECRYPT_MODE, MySignature.publicKey);
-    }
-
-    public static void verify() throws IllegalBlockSizeException, BadPaddingException {
-        byte[] digestAfterSignature = MySignature.getCipher().doFinal(signature);
-
-        for (int i = 0; i < MySignature.getMD().digest().length; i++) {
-            if (digestAfterSignature[i] != MySignature.getMD().digest()[i]) {
-                System.out.println("Assinatura invalida" + '\n');
-                return;
-            }
-        }
-        System.out.println("Assinatura válida" + '\n');
-    }
-
-    public static void sign() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException {
-        System.out.println("Assinando digitalmente a mensagem...");
-
-        StringBuffer hexString = new StringBuffer();
-
-        MySignature.setCipher(Cipher.getInstance("RSA"));
-        MySignature.getCipher().init(Cipher.ENCRYPT_MODE, MySignature.privateKey);
-        MySignature.setSignature(MySignature.getCipher().doFinal(MySignature.getMD().digest()));
-        for (int i = 0; i < MySignature.getSignature().length; i++) {
-            hexString.append(Integer.toHexString(0xFF & MySignature.getSignature()[i]));
-        }
-        System.out.println("Assinatura finalizada: " + hexString.toString() + '\n');
-    }
+    // *******************************************************//
 }
